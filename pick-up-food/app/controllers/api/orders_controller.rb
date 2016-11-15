a5cd923758bf54a187147f6ffd57a7349845d50b
Jacquelyn Yuen <yuencd@gmail.com>
2016-11-15 19:36:44 +0800

    buttons working

--- pick-up-food/app/controllers/api/orders_controller.rb
+++ pick-up-food/app/controllers/api/orders_controller.rb
@@ -1,11 +1,10 @@
 class API::OrdersController < ApplicationController
 
   before_action :authenticate_user!
-
+  before_action :get_orders, only:[:index]
+  before_action :get_order, only:[:update, :destroy]
 
   def index
-    @orders = Order.all.order(:pick_up_time).includes(:order_products, :products)
-    # @orders = Order.all.order(:pick_up_time)
   end
 
   def create
@@ -24,9 +23,34 @@ class API::OrdersController < ApplicationController
 
   end
 
+  def update
+    @order.assign_attributes(prepare_ready_pickedup_params)
+    if @order.save
+
+      render json: @order
+
+    else
+      render json: {message: 'Order Cannot Be Saved'}, status: 404
+    end
+  end
+
 
 private
 
+def get_orders
+  @orders = Order.all.order(:pick_up_time).includes(:order_products, :products)
+end
+
+def get_order
+  @order = Order.find_by(id: params[:id])
+  if @order.nil?
+    render json: {message: "Cannot find Order"}, status: 404
+  end
+end
+
+def prepare_ready_pickedup_params
+  params.permit(:preparing, :ready, :picked_up)
+end
 
 def order_params
   params.permit(:preparing, :ready, :picked_up, :user_id, :pickup_code, :pick_up_time)
